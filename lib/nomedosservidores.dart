77f52fa3e0bd32eafd071fa1a923a7034c28251a
RobertoLPereira <roberto.lpereira65@gmail.com>
2022-07-11 22:21:47 -0300

    Update nomedosservidores.dart
    Nomde dos servidores

--- lib/nomedosservidores.dart
+++ lib/nomedosservidores.dart
@@ -1,5 +1,5 @@
 class NomeServidoresApi {
-  static const Api_Alugueis = 'https://apialugueis.herokuapp.com';
+  static const Api_Alugueis = 'http://10.0.0.109:3050';
   static const servidor = "http://10.0.0.105:3050";
   static const servidorheroku = "https://apialugueis.herokuapp.com";
 }
